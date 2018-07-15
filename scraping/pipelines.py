de4aa193553b71367fc1b27f2e74e48c976733b0
Rami <ramibahrouni47@gmail.com>
2018-07-15 17:26:30 +0100

    added MongoDB settings

--- scraping/pipelines.py
+++ scraping/pipelines.py
@@ -5,6 +5,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exceptions import DropItem
+from scrapy.conf import settings
+from scrapy.exceptions import DropItem
+from scrapy import log
+
+import pymongo
 
 class GPlayPipeline(object):
     def __init__(self):
@@ -18,3 +23,24 @@ class GPlayPipeline(object):
             return item
 
 
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            self.collection.insert(dict(item))
+            log.msg("App added to MongoDB database!",
+                    level=log.DEBUG, spider=spider)
+        return item
--- scraping/settings.py
+++ scraping/settings.py
@@ -13,14 +13,19 @@ BOT_NAME = 'app'
 SPIDER_MODULES = ['scraping.spiders']
 NEWSPIDER_MODULE = 'scraping.spiders'
 #AJAXCRAWL_ENABLED = True
-FEED_FORMAT = 'csv'
-FEED_URI = 'Contacts.csv'
+#FEED_FORMAT = 'csv'
+#FEED_URI = 'Contacts.csv'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Rami Bahrouni (+https://bitbucket.org/rio05/g_play_scraper)'
 ITEM_PIPELINES = {
     'scraping.pipelines.GPlayPipeline': 300,
+    'scraping.pipelines.MongoDBPipeline': 400,
 }
 
+MONGODB_SERVER = "ds018308.mlab.com"
+MONGODB_PORT = 18308
+MONGODB_DB = "gplayapps"
+MONGODB_COLLECTION = "Contacts"
 REACTOR_THREADPOOL_MAXSIZE = 20
 # LOG_LEVEL = 'INFO'
 COOKIES_ENABLED = False
