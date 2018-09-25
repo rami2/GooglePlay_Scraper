e2223fd53a21c09225c5d87855dd2e1b64173852
Rami <ramibahrouni47@gmail.com>
2018-09-25 12:04:54 +0100

    Items organized

--- scraping/items.py
+++ scraping/items.py
@@ -8,10 +8,10 @@ import scrapy
 class GplayItem(scrapy.Item):
 
     App_name = scrapy.Field()
-    Developer = scrapy.Field()
-    Downloads = scrapy.Field()
-    Developer_EMail = scrapy.Field()
     Genre = scrapy.Field()
+    Developer = scrapy.Field()
+    Developer_EMail = scrapy.Field()    
+    Downloads = scrapy.Field()
     Rating_value = scrapy.Field()
     Review_number = scrapy.Field()
 
